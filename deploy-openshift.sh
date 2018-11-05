3deb938c6ad869f07be45dba94dcf51de0c8c622
JorisJamers <jorisjamers22@hotmail.com>
2018-11-05 12:23:37 +0100

    delete get-env-var and using sed command in .sh script

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -8,7 +8,20 @@
 echo "Cloning the git repo on the bastion host"
 git clone https://github.com/JorisJamers/homework-openshift.git
 
-source get-env-var.sh
+# Getting the GUID from the server, this is needed to use in the inventory.
+echo "Getting the GUID"
+export GUID=$(echo $(hostname | awk -F'.' '{ print $2 }'))
+
+# As above, we do the same for the domain name. Afterwards we can use the domain name in the inventory. For this we will use cut because we need
+# every column after the third one.
+echo "Getting the domain name"
+export DOMAIN=$(echo $(hostname | cut -d'.' -f 3-))
+
+# Here we will edit the inventory file with the GUID we got from the bastion host
+sed -i "s/\$GUID/${GUID}/g" ~/homework-openshift/inventory
+
+# Here we will edit the inventory file with the GUID we got from the bastion host
+sed -i "s/\$DOMAIN/${DOMAIN}/g" ~/homework-openshift/inventory
 
 # We will be able to run the ansible-playbook by now. We start with the prerequisites. This is needed to pre-configure the nodes of the cluster.
 echo "Running the prerequisites"
