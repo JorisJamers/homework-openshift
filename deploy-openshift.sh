12ae5ca9bd306f5258873fc2b013df71eb166229
JorisJamers <jorisjamers22@hotmail.com>
2018-11-05 10:43:36 +0100

    running the playbook in deploy-openshift.sh

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -5,17 +5,22 @@
 ################################################################################################
 
 # Cloning the repo into a folder on the bastion host. With this repo we will be able to configure openshift and deploy it.
+echo "Cloning the git repo on the bastion host"
 git clone https://github.com/JorisJamers/homework-openshift.git
 
 # Getting the GUID from the server, this is needed to use in the inventory.
+echo "Getting the GUID"
 export GUID=$(echo $(hostname | awk -F'.' '{ print $2 }'))
 
 # As above, we do the same for the domain name. Afterwards we can use the domain name in the inventory. For this we will use cut because we need
 # every column after the third one.
+echo "Getting the domain name"
 export DOMAIN=$(echo $(hostname | cut -d'.' -f 3-))
 
 # We will be able to run the ansible-playbook by now. We start with the prerequisites. This is needed to pre-configure the nodes of the cluster.
+echo "Running the prerequisites"
 anisble-playbook -i ~/homework-openshift/inventory /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 
-# After we ran the prerequisites we are able to start the deployment of the cluster. 
+# After we ran the prerequisites we are able to start the deployment of the cluster.
+echo "Running the deploy_cluster playbook"
 ansible-playbook -i ~/homework-openshift/inventory /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
