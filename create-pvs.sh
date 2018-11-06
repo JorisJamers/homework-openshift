83166747d2a3738ebade4a6050bc80d93a64dcbb
JorisJamers <jorisjamers22@hotmail.com>
2018-11-06 11:24:45 +0100

    become true and mod creat-pvs.sh

--- create-pvs.sh
+++ create-pvs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 sudo -i
-mkdir -p /srv/nfs/user-vols/pv{1..200}
+mkdir -p /srv/nfs/user-vols/pv{1..50}
 
-for pvnum in {1..50} ; do
-echo /srv/nfs/user-vols/pv${pvnum} *(rw,root_squash) >> /etc/exports.d/openshift-uservols.exports
-chown -R nfsnobody.nfsnobody  /srv/nfs
-chmod -R 777 /srv/nfs
+for pvnum in {1..50} ;
+do
+ echo /srv/nfs/user-vols/pv${pvnum} *(rw,root_squash) >> /etc/exports.d/openshift-uservols.exports
+ chown -R nfsnobody.nfsnobody  /srv/nfs
+ chmod -R 777 /srv/nfs
 done
 
 systemctl restart nfs-server
