211b7c33601edb9f394e5d469ecff1bb8f5e8dfe
JorisJamers <jorisjamers22@hotmail.com>
2018-11-06 07:33:05 +0100

    pvs, project & nodejs-mongo-persistent app

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -38,11 +38,9 @@ echo "Getting the oc command for the bastion host"
 ansible masters[0] -b -m fetch -a "src=/root/.kube/config dest=/root/.kube/config flat=yes"
 
 # Now we log in to the cluster
-
 oc login -u system:admin
 
 # In the following commands we will create the PVS for the users
-
 mkdir -p /srv/nfs/user-vols/pv{1..200}
 
 echo "Create directories at the NFS server to be used as PVs in the OpenShift cluster.."
@@ -54,13 +52,10 @@ for pvnum in {1..50} ; do
 done
 
 # Afterwards we will restart the nfs-server
-
 systemctl restart nfs-server
 
 # Create 25 definition files for 5G PVs
-
-export GUID=`hostname|awk -F. '{print $2}'`
-
+echo "Creating the template for 5G PVs"
 export volsize="5Gi"
 mkdir /root/pvs
 for volume in pv{1..25} ; do
@@ -88,9 +83,7 @@ echo "Created def file for ${volume}";
 done;
 
 # Create 25 definition files for 10G pvs
-
-export GUID=`hostname|awk -F. '{print $2}'`
-
+echo "Creating the template for 10G PVs"
 export volsize="10Gi"
 for volume in pv{26..50} ; do
 cat << EOF > /root/pvs/${volume}
@@ -116,11 +109,17 @@ EOF
 echo "Created def file for ${volume}";
 done;
 
-# Create all the PVs from the definition files
-
+# Create all the PVs from the definition files.
+echo "Creating the PVs for the users"
 cat /root/pvs/* | oc create -f -
 
+# We will create the project for the nodejs-mongo-persistent.
+echo "Creating the project for the nodejs-mongo-persistent application"
+oc new-project smoke-test
 
+# Now we are ready to deploy the application to test our PVs.
+echo "Deploying the nodejs-mongo-persistent application"
+oc new-app nodejs-mongo-persistent
 
 # We are going to create a new project for the jenkins pod.
 oc new-project cicd-dev
